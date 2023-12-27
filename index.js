10182fafcb42a3a96de22bb0d0f7c6243144b6da
CodeMeYulia <yupipper@gmail.com>
2023-12-27 23:46:55 +0800

    check name, check email

--- index.js
+++ index.js
@@ -2,14 +2,61 @@ const form = document.forms.registrationForm;
 const submit = document.querySelector('.submit');
 const cbAgreement = form.elements.cbAgreement;
 
+const inputName = document.getElementById('inputName');
+const inputEmail = document.getElementById('inputEmail');
+const inputAge = document.getElementById('inputAge');
+const inputGender = document.getElementById('gender');
+
+//разблокируем кнопку сабмита при активном чекбоксе
 cbAgreement.addEventListener('change', () => {
     if (cbAgreement.checked === true){
         submit.disabled = false;
+    } else {
+        submit.disabled = true;
     }
 })
 
+function checkName(){
+    let nameRegex = /^[a-zA-Zа-яА-Я ]+$/;
+    const errorName = document.getElementById('errorName');
+    if (nameRegex.test(inputName.value)){
+        alert('Привет,' + inputName.value);
+        errorName.style.display = 'none'
+    } else {
+        errorName.textContent = 'имя некорректно';
+        errorName.style.color = 'red';
+        errorName.style.display = 'block'
+    }
+}
+
+
+function checkEmail(){
+let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const errorEmail = document.getElementById('errorEmail');
+if (emailRegex.test(inputEmail.value)){
+    alert('email валиден');
+} else {
+    errorEmail.textContent = 'укажите валидный email';
+    errorEmail.style.color = 'red';
+    errorEmail.style.display = 'block'
+}
+};
+
+// function checkAge(){
+//     console.log(checkAge);
+// };
 
 form.addEventListener('submit', function(evt){
     evt.preventDefault();
-    console.log('mao');
+    checkName();
+    // checkEmail();
+    const user = {
+        name: inputName.value,
+        email: inputEmail.value,
+        age: inputAge.value,
+        // gender: inputGender.checked.value,
+    }
+    console.log(user);
+    inputName.value = '';
+    inputEmail.value = ''; 
 })
